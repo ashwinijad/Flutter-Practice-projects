60740719c76a50dc1e29674680e777956542d5e6
ashwini.jadhav <ashwini01.jadhav@gmail.com>
2024-07-22 17:33:39 +0530

    initial commit advance flutter quiz app

---
+++ android/app/src/main/kotlin/com/example/advance_flutter_proj/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.advance_flutter_proj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:advance_flutter_proj/quiz.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const Quiz());
+}
+
